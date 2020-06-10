63fd504fc08ac39d49bfe38694f68a2d65b8da62
Alex Lu <Alexlu@office.epicsafety.com>
2020-06-10 14:09:15 -0700

    route

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,59 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Navbar from './Navbar';
-import { Container } from "react-bootstrap";
-import Table from './Table';
-import userInfo from '../info';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Admin from './admin/App';
 
 
 class App extends React.Component {
 
-    
-    state = { info: null, selectedRows: [] }
-
-    componentDidMount() {
-        this.readInfo();
-    }
-
-    readInfo = async() => {
-        this.setState({ info: userInfo });
-    }
-
-    onRowsSelect = (row, select) => {
-        //
-        select ? this.setState({ selectedRows: [...this.state.selectedRows, row] }) : this.setState({ selectedRows: this.state.selectedRows.filter(element => element !== row) });
-    }
-
-
-    renderTable() {
-        if (!this.state.info) return <div>Loading Table...</div> 
-
-        else {
-            return <Table info={this.state.info} onRowsSelect={this.onRowsSelect} />
-        }
-    }
-
     render() {
 
-        console.log(this.state.selectedRows);
-
         return (
-            <Container className="p-2">
-                 <Navbar />
-                 <br />
-                 {this.renderTable()}
-            </Container>
-           
+            <div>
+                <BrowserRouter>
+                    <Route path="/clientAdmin" exact component={Admin} />
+                </BrowserRouter>
+            </div>           
         );
     }
+
+    
+    // state = { info: null, selectedRows: [] }
+
+    // componentDidMount() {
+    //     this.readInfo();
+    // }
+
+    // readInfo = async() => {
+    //     this.setState({ info: userInfo });
+    // }
+
+    // onRowsSelect = (row, select) => {
+    
+    //     select ? this.setState({ selectedRows: [...this.state.selectedRows, row] }) : this.setState({ selectedRows: this.state.selectedRows.filter(element => element !== row) });
+    // }
+
+
+    // renderTable() {
+    //     if (!this.state.info) return <div>Loading Table...</div> 
+
+    //     else {
+    //         return <Table info={this.state.info} onRowsSelect={this.onRowsSelect} />
+    //     }
+    // }
+
+    // render() {
+
+    //     console.log(this.state.selectedRows);
+
+    //     return (
+    //         <Container className="p-2">
+    //              <Navbar />
+    //              <br />
+    //              {this.renderTable()}
+    //         </Container>
+           
+    //     );
+    // }
 }
 
 export default App;
---
+++ src/components/admin/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+//import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Navbar from './Navbar';
+import { Container } from "react-bootstrap";
+import Table from './Table';
+import userInfo from '../../info';
+
+
+class App extends React.Component {
+
+    
+    state = { info: null, selectedRows: [] }
+
+    componentDidMount() {
+        this.readInfo();
+    }
+
+    readInfo = async() => {
+        this.setState({ info: userInfo });
+    }
+
+    onRowsSelect = (row, select) => {
+    
+        select ? this.setState({ selectedRows: [...this.state.selectedRows, row] }) : this.setState({ selectedRows: this.state.selectedRows.filter(element => element !== row) });
+    }
+
+
+    renderTable() {
+        if (!this.state.info) return <div>Loading Table...</div> 
+
+        else {
+            return <Table info={this.state.info} onRowsSelect={this.onRowsSelect} />
+        }
+    }
+
+    render() {
+
+        console.log(this.state.selectedRows);
+
+        return (
+            <Container className="p-2">
+                 <Navbar />
+                 <br />
+                 {this.renderTable()}
+            </Container>
+           
+        );
+    }
+}
+
+export default App;
